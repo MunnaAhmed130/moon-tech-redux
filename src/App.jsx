f19d1f6da6a70bdcf2d1c8c87916687218304050
Munna Ahmed <munnaahmed2025@gmail.com>
2023-01-13 01:54:51 +0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 import "./App.css";
 import routes from "./routes/routes";
 import { RouterProvider } from "react-router";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
     // const [count, setCount] = useState(0);
 
     return (
         // <div className="App">
-        <RouterProvider router={routes} />
+        <Provider store={store}>
+            <RouterProvider router={routes} />
+        </Provider>
         // </div>
     );
 }
---
+++ src/redux/actionTypes/actionTypes.jsx
@@ -0,0 +1,4 @@
+// const actionType = {};
+
+export const ADD_TO_CART = "ADD_TO_CART";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
